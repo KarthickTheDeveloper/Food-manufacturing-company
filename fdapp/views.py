1e7e4a28a18742be5f21cffd65d7c50f6115af40
Karthick <karthick.manimaran@hotmail.com>
2022-06-22 15:16:16 +0530

    fdIndustry 22/6 AN update

--- fdapp/views.py
+++ fdapp/views.py
@@ -4,3 +4,23 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def vendor(request):
+    return render(request, 'vendor.html')
+
+
+def purchase(request):
+    return render(request, 'purchase.html')
+
+
+def tech(request):
+    return render(request, 'tech.html')
+
+
+def production(request):
+    return render(request, 'production.html')
+
+
+def admin(request):
+    return render(request, 'admin.html')
